caaf90158aa800d9f5f50b6d4927e5a2b2abd2dc
Amarnathsetty1999 <amsetty19@gmail.com>
2022-12-06 18:54:56 +0000

    First commit

---
+++ user centered/register.php
@@ -0,0 +1,27 @@
+<?php
+$email=$_POST['email'];
+$psw=$_POST['psw'];
+$name=$_POST['name'];
+$pno=$_POST['pno'];
+
+$pdo=new PDO("mysql:host=localhost;dbname=wta","root","");
+$result=$pdo->query("select * from users where email='$email'"); 
+if(($row=$result->fetch()))
+{
+  echo "Specified email already existss";
+  $pdo=null;
+  exit;
+}
+if($email=='' || $psw=='' || $name==''|| $pno='')
+{
+  $pdo=null;
+  echo("Fields  empty");
+}
+else
+{
+$sql="INSERT into users values('$email','$psw','$name','$pno')";
+$result=$pdo->exec($sql);
+echo "Registered successfully";
+$pdo=null;
+}
+?>
